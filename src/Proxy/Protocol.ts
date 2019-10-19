adbb426aefb04181b67a6635133a4345c1880adc
Anton Nesterov <arch.nesterov@gmail.com>
2019-10-20 02:47:17 +0300

    [tcp proxy] create prototype

--- src/Proxy/Protocol.ts
+++ src/Proxy/Protocol.ts
@@ -4,27 +4,67 @@
 
 //@ts-ignore
 import * as Parser from 'redis-parser'
+//@ts-ignore
+import * as Resp from 'respjs'
 
 export default class Protocol {
     
-    static parse(...buffers : Buffer[]){
+    static decodeRequest(...buffers : Buffer[]){
         const result: any[] = [];
         new Parser({
-            returnReply(reply:any) { result.push(reply) },
-            returnError(err:any) { console.log(err) },
-            returnFatalError(err:any) { console.log(err) }
+            returnReply(reply:any) { 
+                result.push(reply) 
+            },
+            returnError(err:Error) { 
+                result.push({error: 1, message: err.message})
+            },
         }).execute(Buffer.concat(buffers));
         return result
     }
 
+    static encodeRequest(cmds: any[]) {
+        const buffers: any[] = cmds.map((r: any[]) => Resp.encodeRequest(r))
+        return Buffer.concat(buffers)
+    }
+
+    static encodeResponse(resBatch: any[]) {
+        const enc = (val: any): any => {
+
+            if (val === null)
+                return Resp.encodeNull()
+
+            if (val === 'OK')
+                // I don't see more cases
+                return Resp.encodeString(val)
+
+            if (typeof val === 'string')
+                return Resp.encodeBulk(val)
+
+            if (typeof val === 'number')
+                return Resp.encodeInteger(val)
+
+            if (typeof val === 'object' && val.error)
+                return Buffer.from("-" + val.message + "\r\n")
+
+            if (Array.isArray(val) && !val.length)
+                return Resp.encodeNullArray()
+
+            if (Array.isArray(val))
+                return Resp.encodeArray(val.map(enc))
+        }
+        const res: any[] = resBatch.map(enc)
+        return res.join('')
+    }
+
     /**
+     * Convenience method
      * Associate commands with return values, 2D
      * [ [command[], value],
      *  [command[], value]  ]
      * 
      */
     static associate(request: Buffer, response: Buffer) {
-        const result: any[] = Protocol.parse(request, response)
+        const result: any[] = Protocol.decodeRequest(request, response)
         const associate: Array<any[]> = []
         for (let i = 0; i < result.length / 2; i++) {
             associate.push([ result[i], result[i + result.length / 2] ])
@@ -32,8 +72,13 @@ export default class Protocol {
         return associate
     }
 
-    static handle(request: Buffer){
-        const commands: any[] = Protocol.parse(request)
+
+    static isCacheable(cmd: any[]) {
+        switch (cmd[0]) {
+            case 'get':
+                return true
+        }
+        return false
     }
 
 }
--- src/Proxy/TCPProxy.ts
+++ src/Proxy/TCPProxy.ts
@@ -1,10 +1,8 @@
 import IProxy from './IProxy'
-import { RedisClient } from "redis"
 import * as net from 'net'
-//@ts-ignore
-import * as Parser from 'redis-parser'
 
 import Protocol from './Protocol'
+import {get, set} from '../Cache/Worker'
 
 class TCPProxy implements IProxy {
     
@@ -12,21 +10,72 @@ class TCPProxy implements IProxy {
 
     constructor() {}
 
-    handler(requestMessage: Buffer) {
-        const proxySocket: net.Socket = new net.Socket()
+    handler = async (requestMessage: Buffer) => {
         
-        const handleResponse = (request: Buffer) => {
-            proxySocket.on('data', (response: Buffer) => {
-                // console.log('->>>\n', Protocol.associate(request, response))
-                this.socket.write(response)
-                proxySocket.destroy()
+        const sendRequest = (message: Buffer): any => new Promise((resolve, reject)=>{
+            if (!message.length) resolve(message) // return empty buffer
+            const proxySocket: net.Socket = new net.Socket()
+            proxySocket.connect(6379, '127.0.0.1', () => {
+                proxySocket.write(message)
+                proxySocket.on('data', (response: Buffer) => {
+                    resolve(response)
+                    proxySocket.destroy()
+                })
+                proxySocket.on('error', (err) => {
+                    reject(err)
+                    proxySocket.destroy()
+                }) 
             })
-        } 
-
-        proxySocket.connect(6379, '127.0.0.1', () => {
-            proxySocket.write(requestMessage)
-            handleResponse(requestMessage)
         })
+
+        const requestBatch: any[] = Protocol.decodeRequest(requestMessage)
+        let cached: any = {}
+        const filtered: any[] = []
+        
+        for (let i = 0; i < requestBatch.length; i++) {
+            const cmd = requestBatch[i]
+            if (Protocol.isCacheable(cmd)) {
+                const [c, key] = cmd
+                const cache = await get(key)
+                if (cache)
+                    cached[cmd.toString()] = cache
+                else
+                    filtered.push(cmd)
+            } else {
+                filtered.push(cmd)
+            }
+        }
+        const request: Buffer = Protocol.encodeRequest(filtered)
+        const response: Buffer = await sendRequest(request)
+
+        const fromRedis: any = {}
+        Protocol.associate(request, response).map((item: any[]) => {
+            const [cmd, value] = item
+            fromRedis[cmd.toString()] = value
+        })
+
+        const responseBatch = []
+        for (let i = 0; i < requestBatch.length; i++) {
+            const cmd = requestBatch[i].toString()
+            const cachedValue = cached[cmd]
+            if (cachedValue) {
+                //console.log('cached >>>>', cachedValue)
+                responseBatch.push(cachedValue)
+                continue
+            }
+            const redisValue = fromRedis[cmd]
+            if (!Protocol.isCacheable(requestBatch[i])) {
+                responseBatch.push(redisValue)
+                continue
+            }
+            const [c, key] = requestBatch[i]
+            await set(key, redisValue)
+            responseBatch.push(redisValue)
+        }
+
+        //console.log('-=-=-=-=\n', Protocol.encodeResponse(responseBatch).toString(), response.toString())
+        this.socket.write(Protocol.encodeResponse(responseBatch))
+        
         
     }
 
